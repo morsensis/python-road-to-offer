d1e499d3b029ef4861d40d255ad7a905dd8f1f6d
mor1 <morsensis@gmail.com>
2026-03-30 10:07:24 +0300

    Day 2: input, condition, try/except

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-print("28.01 — I started. Deadline: 05.11")
+print('Hello, username. Please, verify your age and name')
+name = input('Input your name:')
+try:
+    age = int(input('How old are u?'))
+    if(age>=18):
+        print("Access granted. Welcome to the project, " + name)
+    elif(age<18):
+        print("Access denied")
+except:
+    print("Verify not clear")
