bc3eb890a6025c177d73b1563b50512b744e823c
mor1 <morsensis@gmail.com>
2026-03-31 12:58:30 +0300

    Day 3: loop improved with while True

--- Main.py
+++ Main.py
@@ -1,10 +1,13 @@
 print('Hello, username. Please, verify your age and name')
 name = input('Input your name:')
-try:
-    age = int(input('How old are u?'))
-    if(age>=18):
-        print("Access granted. Welcome to the project, " + name)
-    elif(age<18):
-        print("Access denied")
-except:
-    print("Verify not clear")
+while True:
+    try:
+        age = int(input('How old are u?'))
+        if(age>=18):
+            print("Access granted. Welcome to the project, ", name)
+        else:
+            print("Access denied")
+        break
+    except ValueError:
+        print("Verify not clear")
+
