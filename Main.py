e31a9d031139ec7dec2feb7cb0362fcae14be15c
mor1 <morsensis@gmail.com>
2026-04-02 11:13:03 +0300

    Day 4: Functions

--- Main.py
+++ Main.py
@@ -1,13 +1,25 @@
 print('Hello, username. Please, verify your age and name')
-name = input('Input your name:')
-while True:
-    try:
-        age = int(input('How old are u?'))
-        if(age>=18):
-            print("Access granted. Welcome to the project, ", name)
-        else:
-            print("Access denied")
-        break
-    except ValueError:
-        print("Verify not clear")
+def get_name():
+    name = input('Input your name:')
+    return name
 
+def get_age():
+    while True:
+        try:
+            age = int(input('How old are u?'))
+            break
+        except ValueError:
+            print("Invalid input")
+        
+    return age
+
+def check_access(age, name): 
+    if(age>=18):
+        print("Access granted. Welcome to the project, ", name)
+    else:
+        print("Access denied")           
+    
+
+name = get_name()
+age = get_age()
+check_access(age, name)
