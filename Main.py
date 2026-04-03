98540fa30680d913404dbd2d0f3224b83dc1ced5
mor1 <morsensis@gmail.com>
2026-04-03 18:52:48 +0300

    Day 4.5: Introduction to lists

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-print('Hello, username. Please, verify your age and name')
+
 def get_name():
     name = input('Input your name:')
     return name
@@ -6,20 +6,28 @@ def get_name():
 def get_age():
     while True:
         try:
-            age = int(input('How old are u?'))
-            break
+            return int(input('How old are u?'))
+            
         except ValueError:
             print("Invalid input")
-        
-    return age
 
 def check_access(age, name): 
-    if(age>=18):
+    if age >= 18:
         print("Access granted. Welcome to the project, ", name)
     else:
         print("Access denied")           
     
-
-name = get_name()
-age = get_age()
-check_access(age, name)
+def main():
+    #print('Hello, username. Please, verify your age and name')
+    #name = get_name()
+    #age = get_age()
+    #check_access(age, name)
+    list = []
+    print(list)
+    list.append("h1st")
+    #list = ["h1st", "nissaba", "mor1"]
+    print(list)
+    list.append("mor1")
+    #list = ["h1st", "nissaba", "mor1"]
+    print(list)
+main() 
